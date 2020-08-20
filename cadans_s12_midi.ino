2eea517a25508296316c81fc6de7e6bb6977edb8
Maxwell Pray <synthead@gmail.com>
2020-08-19 23:07:52 -0700

    Remove debug chord repeat in main loop().

--- cadans_s12_midi.ino
+++ cadans_s12_midi.ino
@@ -5,15 +5,4 @@ void setup() {
 }
 
 void loop() {
-  CadansS12::keys[24] = false;
-  CadansS12::keys[28] = false;
-  CadansS12::keys[31] = false;
-
-  delay(500);
-
-  CadansS12::keys[24] = true;
-  CadansS12::keys[28] = true;
-  CadansS12::keys[31] = true;
-
-  delay(500);
 }
