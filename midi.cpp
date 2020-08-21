616aec13bb1feacf630da7ef86936d3f255ee56f
Maxwell Pray <synthead@gmail.com>
2020-08-21 01:40:46 -0700

    Remove debugging code from midi.cpp.

--- midi.cpp
+++ midi.cpp
@@ -11,16 +11,11 @@ namespace MIDI {
 
   void setup() {
     Serial1.begin(31250);
-    Serial.begin(9600);
   }
 
   void loop() {
     while (Serial1.available() > 0) {
       midi_data = Serial1.read();
-
-      char that[20];
-      sprintf(that, "%x", midi_data);
-      Serial.println(that);
       new_message = midi_data & 0b11110000;
 
       if (new_message >= 0x80 && new_message <= 0xEF) {
@@ -36,7 +31,6 @@ namespace MIDI {
             break;
         }
       } else {
-        Serial.println("here");
         set_key_value(byte_1, midi_data > 0x00);
         byte_1 = -1;
       }
